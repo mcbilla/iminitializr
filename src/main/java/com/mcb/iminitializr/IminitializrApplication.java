058b93361f35cbff01f66272a26fc9d5b8e15f03
Libra_mcb <mcb9141018@outlook.com>
2023-12-26 20:13:27 +0800

    init

---
+++ src/main/java/com/mcb/iminitializr/IminitializrApplication.java
@@ -0,0 +1,13 @@
+package com.mcb.iminitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IminitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IminitializrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcb/iminitializr/IminitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcb.iminitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IminitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
